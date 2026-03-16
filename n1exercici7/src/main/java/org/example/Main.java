52cff1014763b3f063626a1096fefaf9fa39fb66
JoelQuintana <joelqn95@gmail.com>
2026-03-16 13:09:42 +0100

    feat(n1exercici6): sorting a List by their strings lengths reversed

---
+++ n1exercici7/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    static void main() {
+        List<String> words = new ArrayList<>(
+                List.of("coche","casa","uno","y","no")
+        );
+        words.stream().sorted(
+                Comparator.comparing(String::length).reversed()
+                )
+                .forEach(System.out::println);
+    }
+}
